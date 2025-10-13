aaa0310033274f77ea405dd05bc51350ea7043b9
khusainovilas <khusainovilas@yandex.ru>
2025-10-13 21:18:10 +0300

    add IMyTask interface

---
+++ HW3/MyThreadPool/MyThreadPool.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="MyThreadPool.cs" company="khusainovilas">
+// Copyright (c) khusainovilas. All rights reserved.
+// </copyright>
+
+namespace MyThreadPool;
+
+/// <summary>
+/// Main thread pool class
+/// </summary>
+public class MyThreadPool
+{
+    
+}
