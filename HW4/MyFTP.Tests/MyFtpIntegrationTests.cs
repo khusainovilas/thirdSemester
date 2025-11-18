aa7236e9516a08646f1cdac3a98fa3a6bbf0a3e8
khusainovilas <khusainovilas@yandex.ru>
2025-11-19 00:58:17 +0300

    fix ci

--- HW4/MyFTP.Tests/MyFtpIntegrationTests.cs
+++ HW4/MyFTP.Tests/MyFtpIntegrationTests.cs
@@ -1,4 +1,4 @@
-// <copyright file="MyFTPintegrationTests.cs" company="khusainovilas">
+// <copyright file="MyFtpIntegrationTests.cs" company="khusainovilas">
 // Copyright (c) khusainovilas. All rights reserved.
 // </copyright>
 
@@ -13,7 +13,7 @@ using System.Security.Cryptography;
 public class MyFtpIntegrationTests
 {
     private const int Port = 12345;
-    private const string ClientExe = @"..\..\..\..\MyFTP.client\bin\Debug\net9.0\MyFTP.client.exe";
+    private const string ClientExe = @"..\..\..\..\MyFTP.client\bin\Debug\net9.0\MyFTP.Client.exe";
     private const string ServerExe = @"..\..\..\..\MyFTP\bin\Debug\net9.0\MyFTP.exe";
     private const string ServerRoot = @"..\..\..\..\MyFTP";
 
